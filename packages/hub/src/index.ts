3f0fb6e37237a2b942b188e66906b36dfe362beb
Ahmed Etefy <ahmed.etefy12@gmail.com>
2021-03-17 13:00:44 +0100

    Moved SessionFlusher to Session

--- packages/hub/src/index.ts
+++ packages/hub/src/index.ts
@@ -1,8 +1,7 @@
 // eslint-disable-next-line deprecation/deprecation
 export { Carrier, DomainAsCarrier, Layer } from './interfaces';
 export { addGlobalEventProcessor, Scope } from './scope';
-export { Session } from './session';
-export { SessionFlusher } from './sessionflusher';
+export { Session, SessionFlusher } from './session';
 export {
   // eslint-disable-next-line deprecation/deprecation
   getActiveDomain,
